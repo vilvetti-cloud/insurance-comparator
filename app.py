9e25d12898a1fdd26cb112e375d9a412ce785849
vilvetti-cloud <vilvetti@gmail.com>
2026-09-01 17:46:49 +0300

    Update app.py
    fix port

--- app.py
+++ app.py
@@ -17,4 +17,4 @@ app.secret_key = secrets.token_hex(16)
 # ==================== ЗАПУСК ====================
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8501)
+    app.run(host='0.0.0.0', port=5000)
