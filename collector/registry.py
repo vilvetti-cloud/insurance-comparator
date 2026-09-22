24b0ac9ee0a685b7444112347876e549b01d5de2
vilvetti-cloud <vilvetti@gmail.com>
2026-09-22 15:38:08 +0300

    Use official Renaissance CASCO rules PDF

--- collector/registry.py
+++ collector/registry.py
@@ -16,7 +16,7 @@ INSURERS = (
     InsurerConfig("reso", "РЕСО-Гарантия", "РЕСО", "https://reso.ru/", "https://reso.ru/individual/auto/kasko/"),
     InsurerConfig("vsk", "ВСК", "ВСК", "https://vsk.ru/"),
     InsurerConfig("ingos", "Ингосстрах", "Ингосстрах", "https://www.ingos.ru/", "https://cdn.ingos.ru/docs/prav_strakh_ats-2024.pdf"),
-    InsurerConfig("renins", "Ренессанс Страхование", "Ренессанс", "https://www.renins.ru/"),
+    InsurerConfig("renins", "Ренессанс Страхование", "Ренессанс", "https://www.renins.ru/", "https://www.renins.ru/Media/Default/doc/rules/157.pdf"),
     InsurerConfig("alfa", "АльфаСтрахование", "Альфа", "https://www.alfastrah.ru/", "https://www.alfastrah.ru/individuals/auto/kasko/"),
     InsurerConfig("soglasie", "Согласие", "Согласие", "https://www.soglasie.ru/"),
     InsurerConfig("rgs", "Росгосстрах", "РГС", "https://www.rgs.ru/", "https://www.rgs.ru/auto/ekasko/kasko-ot-ugona-i-gibeli"),
