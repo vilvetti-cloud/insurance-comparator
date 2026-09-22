c05ab05b40a4d14023e187d91873e919ac12fb63
vilvetti-cloud <vilvetti@gmail.com>
2026-09-22 14:30:29 +0300

    Use official Ingosstrakh CASCO rules

--- collector/registry.py
+++ collector/registry.py
@@ -15,7 +15,7 @@ class InsurerConfig:
 INSURERS = (
     InsurerConfig("reso", "РЕСО-Гарантия", "РЕСО", "https://reso.ru/", "https://reso.ru/individual/auto/kasko/"),
     InsurerConfig("vsk", "ВСК", "ВСК", "https://vsk.ru/"),
-    InsurerConfig("ingos", "Ингосстрах", "Ингосстрах", "https://www.ingos.ru/"),
+    InsurerConfig("ingos", "Ингосстрах", "Ингосстрах", "https://www.ingos.ru/", "https://cdn.ingos.ru/docs/prav_strakh_ats-2024.pdf"),
     InsurerConfig("renins", "Ренессанс Страхование", "Ренессанс", "https://www.renins.ru/"),
     InsurerConfig("alfa", "АльфаСтрахование", "Альфа", "https://www.alfastrah.ru/", "https://www.alfastrah.ru/individuals/auto/kasko/"),
     InsurerConfig("soglasie", "Согласие", "Согласие", "https://www.soglasie.ru/"),
